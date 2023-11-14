edb08cc2b019f9653f0a64a27cd5f3630ddc7e97
rendrabimaa <rendrabimaa@gmail.com>
2023-11-14 11:57:19 +0700

    implement Link & Navigation (UI Register & Login Not Complete)

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,20 +1,27 @@
 import Link from "next/link"
+import { usePathname, useRouter } from "next/navigation"
 
 const Navbar = () => {
+  const pathname = usePathname()
+  const router = useRouter()
   return (
     <nav className="flex bg-gray-800 py-2 px-5">
+      <div className="flex justify-between w-full">
       <h1 className="text-white">Navbar</h1>
       <ul className="flex">
         <Link href={"/"}>
-          <li className="mr-3 text-blue-300 cursor-pointer">Home</li>
+          <li className={`mr-3 ${pathname === '/' ? 'text-blue-300' : 'text-white'} cursor-pointer`}>Home</li>
         </Link>
         <Link href={"/about"}>
-          <li className="mr-3 text-blue-300 cursor-pointer">About</li>
+          <li className={`mr-3 ${pathname === '/about' ? 'text-blue-300' : 'text-white'} cursor-pointer`}>About</li>
         </Link>
         <Link href={"/about/profile"}>
-          <li className="mr-3 text-blue-300 cursor-pointer">Profile</li>
+          <li className={`mr-3 ${pathname === '/about/profile' ? 'text-blue-300' : 'text-white'} cursor-pointer`}>Profile</li>
         </Link>
       </ul>
+      <button className="bg-white rounded-md px-3 h-7"
+      onClick={() => router.push("/login")}>Login</button>
+      </div>
     </nav>
   )
 }
